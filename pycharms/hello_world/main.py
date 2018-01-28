44b2697fd90457c090fe26124be9b678c49e1946
Stephen Christopher Adams <vengeanz@gmail.com>
2018-01-28 15:51:55 +1300

    Had a go at tail recursion and string slicing

--- pycharms/hello_world/main.py
+++ pycharms/hello_world/main.py
@@ -1,2 +1,21 @@
-me = "Steve"
-print(f"Hello {me}")
+def recursive_reverse_string(s: str):
+    def tail_helper(inner_s: str, accumulated: str):
+        if len(inner_s) <= 0:
+            return accumulated
+        return tail_helper(inner_s[:-1], accumulated + inner_s[-1:])
+
+    return tail_helper(s, "")
+
+
+def reverse_string(s: str):
+    return s[::-1]
+
+
+if __name__ == "__main__":
+    me = "steve"
+    print(f"Hello {reverse_string(me)}")
+
+
+
+
+
